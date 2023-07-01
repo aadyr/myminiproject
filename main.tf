16dad63fa6959d9f845e097f481dda6ac0018971
aadyr <aadharsh11@gmail.com>
2023-07-01 13:02:51 +0800

    test

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "instances" {
     Name = "aadywebserver-1"
   }
 }
-resource "aws_instance" "instances" {
+resource "aws_instance" "instance1" {
   ami           = "ami-0fc61db8544a617ed"  # Amazon Linux 2 LTS
   instance_type = "t2.micro"
   count         = 1
@@ -22,7 +22,7 @@ resource "aws_instance" "instances" {
     Name = "aadywebserver-2"
   }
 }
-resource "aws_instance" "instances" {
+resource "aws_instance" "instance2" {
   ami           = "ami-0fc61db8544a617ed"  # Amazon Linux 2 LTS
   instance_type = "t2.micro"
   count         = 1
