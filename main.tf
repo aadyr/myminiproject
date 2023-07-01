5d256c9e00eba298271a7a6b136ee4a990139673
aadyr <aadharsh11@gmail.com>
2023-07-01 13:17:35 +0800

    test2

--- main.tf
+++ main.tf
@@ -1,34 +1,23 @@
 resource "aws_instance" "instances" {
+  count = 2
   ami           = "ami-0fc61db8544a617ed"  # Amazon Linux 2 LTS
   instance_type = "t2.micro"
-  count         = 1
   associate_public_ip_address = "true"
-  vpc_security_group_ids = "sg-09b913b1a06a3a22e"
-  subnet_id = "subnet-0c367963f6cb16292"
+  vpc_security_group_ids = "sg-d6a22a98"
+  subnet_id = "subnet-50adb50b"
 
   tags = {
-    Name = "aadywebserver-1"
+    Name = "aadywebserver-${count.index + 1}"
   }
 }
-resource "aws_instance" "instance1" {
-  ami           = "ami-0fc61db8544a617ed"  # Amazon Linux 2 LTS
-  instance_type = "t2.micro"
-  count         = 1
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = "sg-09b913b1a06a3a22e"
-  subnet_id = "subnet-0c367963f6cb16292"
 
-  tags = {
-    Name = "aadywebserver-2"
-  }
-}
 resource "aws_instance" "instance2" {
   ami           = "ami-0fc61db8544a617ed"  # Amazon Linux 2 LTS
   instance_type = "t2.micro"
   count         = 1
   associate_public_ip_address = "true"
-  vpc_security_group_ids = "sg-09b913b1a06a3a22e"
-  subnet_id = "subnet-0c367963f6cb16292"
+  vpc_security_group_ids = "sg-d6a22a98"
+  subnet_id = "subnet-50adb50b"
 
   user_data = <<EOF
      #!/bin/bash
